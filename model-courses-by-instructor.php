5516458e417200d3ccb6cc559b31a36ee4ce6c56
Gaines2025 <maalakai.gaines@ou.edu>
2024-10-01 14:20:52 -0500

    Create model-courses-by-instructor.php

---
+++ model-courses-by-instructor.php
@@ -0,0 +1,16 @@
+<?php
+function selectCoursesByInstructor() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT c.course_id, course_number, course_description, semester, room, day_time FROM `course` c join section s on s.course_ID = c.course_ID where s.instructor_ID=1");
+        $stmt->bind_param("i", $iid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
