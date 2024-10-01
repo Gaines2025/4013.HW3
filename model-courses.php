53d5ffc7333fb12304f04bdf6cca4cdfc4b3f06d
Gaines2025 <maalakai.gaines@ou.edu>
2024-10-01 14:36:15 -0500

    Update model-courses.php

--- model-courses.php
+++ model-courses.php
@@ -1,5 +1,5 @@
 <?php
-function selectInstructors() {
+function selectCourses() {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT course_id, course_name, course_description FROM `course`");
