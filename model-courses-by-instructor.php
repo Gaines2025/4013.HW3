1183e74515172737aa459953ea932c18ec650ba2
Gaines2025 <maalakai.gaines@ou.edu>
2024-10-01 15:02:48 -0500

    Update model-courses-by-instructor.php

--- model-courses-by-instructor.php
+++ model-courses-by-instructor.php
@@ -1,5 +1,5 @@
 <?php
-function selectCoursesByInstructor() {
+function selectCoursesByInstructor($iid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT c.course_id, course_number, course_description, semester, room, day_time FROM `course` c join section s on s.course_ID = c.course_ID where s.instructor_ID=1");
