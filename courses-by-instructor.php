19df8a161a421faa04b3e17ea65535ed08df331e
Gaines2025 <maalakai.gaines@ou.edu>
2024-10-02 11:13:40 -0500

    Update courses-by-instructor.php

--- courses-by-instructor.php
+++ courses-by-instructor.php
@@ -4,7 +4,7 @@ require_once("model-courses-by-instructor.php");
 
 $pageTitle = "Courses By Instructor";
 include "view-header.php";
-$courses = selectCoursesByInstructor($_GET['id']);
+$courses = selectCoursesByInstructor($_GET['ID']);
 include "view-courses-by-instructor.php";
 include "view-footer.php";
 ?>
