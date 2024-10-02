e94bb6342c64380e4eaae224b75fec6ed09ca2fa
Gaines2025 <maalakai.gaines@ou.edu>
2024-10-02 12:05:20 -0500

    Update model-instructors-with-courses.php

--- model-instructors-with-courses.php
+++ model-instructors-with-courses.php
@@ -12,4 +12,19 @@ function selectInstructors() {
         throw $e;
     }
 }
+
+function selectCoursesByInstructor($iid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT c.course_ID, course_number, course_description, semester, room, day_time FROM `course` c join section s on s.course_ID = c.course_ID where s.instructor_ID=?");
+        $stmt->bind_param("i", $iid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
