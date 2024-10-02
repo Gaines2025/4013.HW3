6bc0dd58eedf66c6269a8152cfa9db9b387951bc
Gaines2025 <maalakai.gaines@ou.edu>
2024-10-02 10:50:11 -0500

    Update view-courses-by-instructor.php

--- view-courses-by-instructor.php
+++ view-courses-by-instructor.php
@@ -19,6 +19,9 @@ while ($course = $courses->fetch_assoc()) {
     <td><?php echo $course['course_id']; ?></td>
     <td><?php echo $course['course_number']; ?></td>
     <td><?php echo $course['course_description']; ?></td>
+    <td><?php echo $course['semester']; ?></td>
+    <td><?php echo $course['room']; ?></td>
+    <td><?php echo $course['day_time']; ?></td>
   </tr>
 <?php
 }
